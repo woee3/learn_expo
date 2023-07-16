fe6c9a2c2ab171a27a317f504194e4b1b696a406
Woo <kimhyn21@gmail.com>
2023-07-16 22:05:25 +0900

    learning

---
+++ StickerSmash/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <Text style={{color: '#fff'}}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+});
---
+++ StickerSmash/components/Button.js
@@ -0,0 +1,20 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+
+export default function Button({label}) {
+    return (
+        <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+                <Text style={styles.buttonLabel}>{label}</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: 320,
+        height: 68,
+        marginHorizontal: 20,
+        
+    }
+})
