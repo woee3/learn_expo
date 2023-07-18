3f23691e2a1b267f10979388011cf711b5a32160
Woo <kimhyn21@gmail.com>
2023-07-18 21:31:53 +0900

    7/18

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,19 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer 
+        placeholderImageSource={PlaceholderImage} 
+        selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a  photo"/>
+        <Button theme="primary" label="Choose a  photo" onPress={pickImageAsync}/>
         <Button label="Use this photo"/>
       </View>
       <Text style={{color: '#fff'}}>Open up App.js to start working on your app!</Text>
